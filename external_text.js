049cd7ab984ef7afe1654a2cb4c01234f733b360
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-01-13 12:19:13 -0800

    Update external_text.js

--- external_text.js
+++ external_text.js
@@ -1,5 +1,5 @@
 /*this is for putting text files in multipule html pages*/
 function test(){
     var text = "test_text";
-    document.getElementById("test").innerHTML = "test_text.txt";
+    document.getElementById("test").innerHTML = test_text.txt;
 }
