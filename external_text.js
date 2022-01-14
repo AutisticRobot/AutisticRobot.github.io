83db665adb397be8b9dfa69e49e18d1395ace87d
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-01-14 13:16:17 -0800

    Update external_text.js

--- external_text.js
+++ external_text.js
@@ -1,9 +1,9 @@
 /*this is for putting text files in multipule html pages*/
 var fs = require("fs");
 fs.readFile("test_text.txt", function(text){
-    var test = text.split("\n")
+    var test = text.split("\n");
 });
 function test(){
     var text = "test_text";
     document.getElementById("test").innerHTML += test;
-}
+};
