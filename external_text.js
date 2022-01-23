7ea6ca2d8ca3cdbf85f6236509ccfe58c9dd924f
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2022-01-23 14:22:30 -0800

    added "game"

--- external_text.js
+++
@@ -1,9 +0,0 @@
-/*this is for putting text files in multipule html pages*/
-var fs = require("fs");
-fs.readFile("test_text.txt", function(text){
-    var test = text.split("\n");
-});
-function fTest(){
-    var text = "test_text";
-    document.getElementById("test").innerHTML += text;
-};
