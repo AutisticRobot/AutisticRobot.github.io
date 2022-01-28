6b82ebbe32b6a66e8b058c154fc48d4a901369c7
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-01-28 10:13:34 -0800

    Update gameCode.js

--- 1stTry/gameCode.js
+++ 1stTry/gameCode.js
@@ -76,6 +76,9 @@ var keys = {
 //movements
 
 function movePlayer(event) {
+  if([32, 37, 38, 39, 40].indexOf(event.keyCode) > -1) {
+        event.preventDefault();
+    }
   switch (event.keyCode) {
     case 39:
       keys["right"] = true;
