7065b3e9b1a8015d5801b2da4e302b770c81f3eb
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2022-02-08 20:43:52 -0800

    finished flappy bot

--- flappyBot/roboticGameEngine.js
+++ flappyBot/roboticGameEngine.js
@@ -191,11 +191,15 @@ function move(id){
 //universial updater
 function uniUpdate(gForce, airResist, buffer){
   for(var c=0; c < hitbox.tag.length; c++){
-    gravity(c, gForce);
-    momentium(c, airResist);
-    move(c);
-    despawn(c, buffer)
-    render(c);
+    switch(c){
+      default:
+      gravity(c, gForce);
+      momentium(c, airResist);
+      move(c);
+      despawn(c, buffer)
+      render(c);
+      break;
+    }
   }
 }
 
