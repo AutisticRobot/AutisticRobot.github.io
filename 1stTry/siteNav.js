45e19b3800237f505ef4c885f0cc2ca3af0ef2e8
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-02-24 13:11:20 -0800

    added tabs to all pages

---
+++ 1stTry/siteNav.js
@@ -0,0 +1,23 @@
+const tabs = document.querySelectorAll('[data-tab-target]');
+
+if(window.localStorage.getItem('ClickTab') == undefined || null){
+    const global = {
+        return: false,
+        clickedTab: "none",
+    }
+    window.localStorage.setItem('ClickTab', JSON.stringify(global));
+  }
+
+
+  //per click update
+  tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        var local = JSON.parse(window.localStorage.getItem('ClickTab'));
+        local.clickedTab = JSON.stringify(tab.getAttribute("data-tab-target"));
+        local.return = true;
+        window.localStorage.setItem('ClickTab', JSON.stringify(local));
+        console.log(local.clickedTab);
+        window.location.href = "/index.html";
+      })
+  })
+  
