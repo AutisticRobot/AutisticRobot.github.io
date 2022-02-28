ea457cf5b2fd72f4afc5e9dcea0e9b2c49d4898a
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2022-02-27 22:38:55 -0800

    fixed non-return page laoding

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ const tabs = document.querySelectorAll('[data-tab-target]');
 const tabContents = document.querySelectorAll('[data-tab-content]');
 var active;
 var local = JSON.parse(window.localStorage.getItem('ClickTab'));
-if(local.return == false){
-if(JSON.parse(window.localStorage.getItem('ClickTab')) == undefined || null){
+if(local == undefined || null || local.clickedTab == undefined || null){
+
     const global = {
         return: false,
         clickedTab: "#home",
     }
     window.localStorage.setItem('ClickTab', JSON.stringify(global));
-}
 }
 
 //per click update
@@ -17,22 +16,34 @@ tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         var local = JSON.parse(window.localStorage.getItem('ClickTab'));
         local.clickedTab = JSON.stringify(tab.getAttribute("data-tab-target"));
-        local.return = true;
         window.localStorage.setItem('ClickTab', JSON.stringify(local));
         setAct(local.clickedTab);
     })
 })
 
 //on load, load clicked tab
-    var local = JSON.parse(window.localStorage.getItem('ClickTab'));
-    if(local.return = true){
-        setAct(local.clickedTab);
-        local.return = false;
-        window.localStorage.setItem('ClickTab', JSON.stringify(local));
-    }
+if(local.return == true){
+    setAct(local.clickedTab);
+    local.return = false;
+    window.localStorage.setItem('ClickTab', JSON.stringify(local));
+}else{
+    tabContents.forEach(tabContent => {
+        tabContent.classList.remove('active');
+    })
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    })
+    tabs.forEach(test => {
+        if (test == tabs[0]){
+            const target = document.querySelector(test.dataset.tabTarget);
+            test.classList.add('active');
+            target.classList.add('active');
+        }
+    })
+}
 
 function setAct(tabAt) {
-    var tab;
+    console.log(tabAt)
     tabContents.forEach(tabContent => {
         tabContent.classList.remove('active');
     })
@@ -41,10 +52,9 @@ function setAct(tabAt) {
     })
     tabs.forEach(test => {
         if (JSON.stringify(test.getAttribute("data-tab-target")) == tabAt){
-            tab = test;
+            const target = document.querySelector(test.dataset.tabTarget);
+            test.classList.add('active');
+            target.classList.add('active');
         }
     })
-    const target = document.querySelector(tab.dataset.tabTarget);
-    tab.classList.add('active');
-    target.classList.add('active');
 }
