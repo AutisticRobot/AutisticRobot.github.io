b63168288cdd762ec4e39977ebb5efcad6c81557
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-04-19 09:29:52 -0700

    added 1 line of code

--- flappyBot/roboticGameEngine.js
+++ flappyBot/roboticGameEngine.js
@@ -5,7 +5,7 @@ canvas.focus();
 const width = canvas.width;
 const height = canvas.height;
 const img = document.getElementById('face');
-//const cloud = document.getElementById('cloud');
+const cloud = document.getElementById('cloud');
 body.addEventListener("keydown", event=>{
   if(event.keyCode != 17 && 16){
     keyAdd(event);
