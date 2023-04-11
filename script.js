82426adb3780ae8abc08c00306b0ffc680da83b8
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-04-10 20:28:42 -0700

    Consolodated site navigation code

--- script.js
+++ script.js
@@ -1,47 +1,8 @@
-
-class siteNav extends HTMLElement 
-{
-    constructor()
-    {
-        super()
-        this.innerHTML =
-        `
-        <nav style="padding-top: 16px;">
-            <ul class="tabs">
-                <li data-tab-target="#home" class="tab active">Home</li>
-                <li data-tab-target="#blog" class="tab">Blog</li>
-                <li data-tab-target="#games" class="tab">Games</li>
-            </ul>
-        </nav>
-        `
-    }
-}
-customElements.define("site-nav", siteNav)//NEEDS to appear BEFORE navigation logic or it wont be detected.
-
-const tabs = document.querySelectorAll(".tabs")[0].childNodes;
 const tabContents = document.querySelectorAll('[data-tab-content]');
 const params = new URLSearchParams(window.location.search);
 var active;
 var local = params.get("local");
 var clicked;
-
-//per click update
-tabs.forEach(tab => {
-    if(tab.classList != undefined)
-    {
-        tab.addEventListener('click', () => {
-
-            for(var t = 1; t < tabs.length; t += 2){
-                if(tab == tabs[t]){
-                    clicked = t;
-                }
-            }
-            let URL=window.location.origin + window.location.pathname + "?local=" + clicked;
-
-            window.location.assign(URL);
-        })
-    }
-})
 
 //on load, load clicked tab
 if(params.has("local")){
